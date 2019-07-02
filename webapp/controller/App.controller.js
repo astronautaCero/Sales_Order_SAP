f0fc942f7da79ee577c83d95ca4653fb2971ab4a
jffersosa@gmail.com <jffersosa@gmail.com>
2019-07-02 21:18:46 +0000

    Commit Prueba

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function (Controller) { 
 	"use strict";
-
+//prueba jfferson
 	return Controller.extend("com.perceptio.training.sales_orders_info.controller.App", {
 		onInit: function () {
 
